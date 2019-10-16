b698ca6173345c9a14c8a187628139218b0e856c
Lewis Dean <ldean@ukcloud.com>
2019-10-16 12:58:58 +0100

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import SteamWorkshopItem from './SteamWorkshopItem/SteamWorkshopItem';
 class App extends Component {
   render() {
     return (
+
       <div className="App">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link>
         <SteamWorkshopItem
           clickAction='clickAction'
-          image={'image'}
+          image={'/logo192.png'}
           title={'title'}
           summary={'summary'}
           views={'views'}
@@ -16,7 +18,7 @@ class App extends Component {
           favourites={'favourites'}/>
         <SteamWorkshopItem
           clickAction='clickAction'
-          image={'image'}
+          image={'/logo192.png'}
           title={'title'}
           summary={'summary'}
           views={'views'}
--- src/SteamWorkshopItem/SteamWorkshopItem.js
+++ src/SteamWorkshopItem/SteamWorkshopItem.js
@@ -1,18 +1,23 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import './SteamWorkshopItem.css'
 
 const steamWorkshopItem = (props) => {
   return (
-    <div className="SteamWorkshopItem" onClick={props.clickAction}>
-      <p> I'm a Steam Workshop item!</p>
-      <img src={props.image} />
-      <p>{props.title}</p>
-      <p>{props.summary}</p>
-      <p>{props.views}</p>
-      <p>{props.subscribers}</p>
-      <p>{props.favourites}</p>
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="bottom" src={props.image} />
+      <Card.Body>
+        <Card.Title><p> I'm a Steam Workshop item!{props.title}</p></Card.Title>
+        <Card.Text>
+          <p>{props.summary}</p>
+          <p>{props.views}</p>
+          <p>{props.subscribers}</p>
+          <p>{props.favourites}</p>
+        </Card.Text>
+        <Button onClick={props.clickAction} variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
 }
 
