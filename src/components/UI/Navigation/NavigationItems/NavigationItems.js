216d63ce7bad7342dcfcff3a5da56ccf4c4b20a6
Lewis Dean <ldean@ukcloud.com>
2020-01-21 13:53:23 +0000

    Fill out nav items(s) and toolbar

--- src/components/UI/Navigation/NavigationItems/NavigationItems.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItems.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {Row} from 'react-bootstrap';
+import NavigationItem from './NavigationItem/NavigationItem';
+import classes from './NavigationItems.module.scss';
+
 
 class NavigationItems extends Component {
 
@@ -7,7 +10,12 @@ class NavigationItems extends Component {
     render() {
         return (
             <Row>
-
+                <ul className={classes.NavigationItems}>
+                    <NavigationItem link="/" active>one</NavigationItem>
+                    <NavigationItem link="/">two</NavigationItem>
+                    <NavigationItem link="/">three</NavigationItem>
+                    <NavigationItem link="/">four</NavigationItem>
+                </ul>
             </Row>
         );
     }
