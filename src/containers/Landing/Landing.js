6f30d30a32f9f8d9113196504c9b1458af2cba86
Lewis Dean <ldean@ukcloud.com>
2020-01-21 12:17:01 +0000

    Use more bootstrap

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import BannerWelcome from '../../components/BannerTexts/BannerWelcome/BannerWelcome';
 import TextCentrepiece from '../../components/Texts/TextCenterpiece/TextCenterpiece';
+import {Row, Col} from 'react-bootstrap';
 
 const landing = (props) => {
     return (
         <Container-fluid>
-            <p>Sidebar, Nav</p>
-            <BannerWelcome text={"Lewis Dean"}></BannerWelcome>
-            <TextCentrepiece text={"can a file or env var go here?"}></TextCentrepiece>
+            <Row>
+                <Col>
+                    <BannerWelcome text={"Lewis Dean"}></BannerWelcome>
+                </Col>
+
+            </Row>
+            <Row>
+                <Col>
+                    <TextCentrepiece text={"can a file or env var go here?"}></TextCentrepiece>
+                </Col>
+            </Row>
         </Container-fluid>
     )
 }
