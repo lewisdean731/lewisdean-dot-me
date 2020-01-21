486cefc56403ca2048a9d6efbd8e9d6fa09950d4
Lewis Dean <ldean@ukcloud.com>
2020-01-21 13:23:53 +0000

    Add NavigationItem(s) components

---
+++ src/components/UI/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Row} from 'react-bootstrap';
+
+class NavigationItems extends Component {
+
+
+    render() {
+        return (
+            <Row>
+
+            </Row>
+        );
+    }
+}
+
+export default NavigationItems;
