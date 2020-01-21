e2cd56079a4961a43dcb7b8c22662298c1b7224c
Lewis Dean <ldean@ukcloud.com>
2020-01-21 15:43:31 +0000

    More styling

--- src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import classes from './NavigationItem.module.scss';
 import { Col } from 'react-bootstrap';
+import Aux from '../../../../../hoc/Aux';
 
 const navigationItem = (props) => (
-        <Col className={classes.NavigationItem}>
+    <Aux>
+        <Col sm="2" className={classes.NavigationItem}>
             <a
                 href={props.link}
                 className={props.active ? classes.active : null}
@@ -11,6 +13,8 @@ const navigationItem = (props) => (
                 {props.children}
             </a>
         </Col>
+    </Aux>
+
 );
 
 export default navigationItem
