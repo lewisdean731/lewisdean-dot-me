2b082eaa24c1667538d97713f7b8653438bff5fd
Lewis Dean <ldean@ukcloud.com>
2020-02-04 12:42:53 +0000

    Spruce up toolbar

--- src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,7 +4,7 @@ import { Col } from 'react-bootstrap';
 import Aux from '../../../../../hoc/Aux';
 
 const navigationItem = (props) => (
-    <Col sm="2" className={classes.NavigationItem}>
+    <Col sm={1} className={classes.NavigationItem}>
         <a
             href={props.link}
             className={props.active ? classes.active : null}
