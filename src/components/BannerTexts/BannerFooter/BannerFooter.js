81e33eef78cc95b053b005ae45f318375f14aa87
Lewis Dean <ldean@ukcloud.com>
2020-03-02 15:36:22 +0000

    Impove footer

--- src/components/BannerTexts/BannerFooter/BannerFooter.js
+++ src/components/BannerTexts/BannerFooter/BannerFooter.js
@@ -9,16 +9,16 @@ const BannerFooter = props => {
 <footer className={[Classes.Footer, Classes.Box].join(" ")}>
   <Row>
    <Col>
-      <a className={Classes.Icon}>
+      <a className={Classes.Icon} href="#">
         <FontAwesomeIcon icon={['fab', 'github']}/>
       </a>
-      <a className={Classes.Icon}>
+      <a className={Classes.Icon} href="#">
         <FontAwesomeIcon icon={['fab', 'steam']}/>
       </a>
-      <a className={Classes.Icon}>
+      <a className={Classes.Icon} href="#">
         <FontAwesomeIcon icon={['fab', 'discord']}/>
       </a>
-      <a className={Classes.Icon}>
+      <a className={Classes.Icon} href="#">
         <FontAwesomeIcon icon={'cube'}/>
       </a>
     </Col>
