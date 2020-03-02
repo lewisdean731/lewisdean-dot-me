be3fc72132aa70205b57e1948f07dc533820f518
Lewis Dean <ldean@ukcloud.com>
2020-03-02 14:49:34 +0000

    Add footer banner component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import Layout from "./containers/Layout/Layout";
 import Aux from "./hoc/Aux";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCube } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faCube)
+
 
 class App extends Component {
   render() {
--- src/components/BannerTexts/BannerFooter/BannerFooter.js
+++ src/components/BannerTexts/BannerFooter/BannerFooter.js
@@ -1,14 +1,34 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import Classes from "./BannerFooter.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const BannerHeader = props => {
+const BannerFooter = props => {
   return (
-    <Row className={[Classes.Title, Classes.Box].join(" ")}>
-      <p>Copyright 2020 Lewis Dean</p>
-    </Row>
 
+<footer className={[Classes.Footer, Classes.Box].join(" ")}>
+  <Row>
+   <Col>
+      <a className={Classes.Icon}>
+        <FontAwesomeIcon icon={['fab', 'github']}/>
+      </a>
+      <a className={Classes.Icon}>
+        <FontAwesomeIcon icon={['fab', 'steam']}/>
+      </a>
+      <a className={Classes.Icon}>
+        <FontAwesomeIcon icon={['fab', 'discord']}/>
+      </a>
+      <a className={Classes.Icon}>
+        <FontAwesomeIcon icon={'cube'}/>
+      </a>
+    </Col>
+  </Row>
+  <div class="footer-copyright text-center py-3">
+    <p>© 2020 Copyright</p>
+    <p>Lewis Dean</p>
+  </div>
+</footer>
   );
 };
 
-export default BannerHeader;
+export default BannerFooter;
