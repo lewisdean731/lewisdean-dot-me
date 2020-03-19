f5fd2fb6d4dea11e199eb44fbafeec3ace210df0
Lewis Dean <ldean@ukcloud.com>
2020-03-19 13:21:24 +0000

    Update about page

--- src/containers/Layout/About/About.js
+++ src/containers/Layout/About/About.js
@@ -3,6 +3,8 @@ import { Row, Col } from "react-bootstrap";
 import TextSubheader from "../../../components/Texts/TextSubheader/TextSubheader";
 import TextHeader from "../../../components/Texts/TextHeader/TextHeader";
 import TextParagraph from "../../../components/Texts/TextParagraph/TextParagraph";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Classes from "./About.module.scss";
 import Aux from "../../../hoc/Aux";
 
 const about = () => {
@@ -19,43 +21,30 @@ const about = () => {
         </Col>
       </Row>
       <Row>
+        <Col></Col>
         <Col>
-          <TextParagraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+          <TextParagraph FullWidth>
+            Originally from Cornwall, I moved to Bournemouth to study Software
+            Engineering in 2017. I'm currently working as part of an agile
+            development team at UKCloud for my industrial placement year, and
+            thoroughly enjoying it! I am passionate about the work I do, as well
+            as the technologies we all use to drive change in the industry.
+          </TextParagraph>
+          <TextParagraph FullWidth>
+            Through university, work, and personal development time I have
+            gained experience using a variety of different languages, tools and
+            technologies, as varied as TypeScript and React to Bash / Shell
+            scripting with Jenkins pipelines and Red Hat SSO. I also have
+            experience with the atlassian suite of tools (Bitbucket, Bamboo,
+            Confluence, and Jira).
           </TextParagraph>
         </Col>
-      </Row>
-      <Row>
         <Col>
-          <TextParagraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </TextParagraph>
+          <p className={[Classes.ProfilePicture, Classes.Placeholder].join(" ")} href="#">
+            <FontAwesomeIcon icon={'user'}/>
+          </p>
         </Col>
-      </Row>
-      <Row>
-        <Col>
-          <TextParagraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </TextParagraph>
-        </Col>
+        <Col></Col>
       </Row>
     </Aux>
   );
