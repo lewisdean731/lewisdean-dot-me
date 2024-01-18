068acd2c3af33c0dee028490093ece40890c4694
Lewis Dean <34524126+lewisdean731@users.noreply.github.com>
2024-01-18 00:43:29 +0000

    wip: experiment with nextjs framework

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      light: '#fefcfb',
+      blue1: '#1282a2',
+      blue2: '#034078',
+      blue3: '#001f54',
+      blue4: '#0a1128',
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
